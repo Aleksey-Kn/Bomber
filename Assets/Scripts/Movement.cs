3c427705f3a17f295d5d57bbdc5f992b373b6d77
Aleksey-Kn <aleksey.k2000@mail.ru>
2021-03-26 00:15:47 +0700

    Realise generate towers

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,12 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private float speed = 1.5f;
+    public float speed = 1.5f;
     void FixedUpdate()
     {
-        transform.Translate(Vector3.left * speed * Time.deltaTime);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if(transform.position.x < -10)
+        {
+            Destroy(gameObject);
+        }
     }
 }
