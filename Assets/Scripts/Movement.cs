a7d6365278b83376b758d4ba51bd99443c660f98
Aleksey-Kn <aleksey.k2000@mail.ru>
2021-03-26 22:08:51 +0700

    Can create bombs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,7 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float speed = 1.5f;
+    private float speed = 2.5f;
     void FixedUpdate()
     {
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
